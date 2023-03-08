fc9d05535c60411ca7fa62d3a8fc11ecf6f903b0
RionelxSlovesky <misteradnan1@gmail.com>
2023-03-08 07:52:33 +0600

    Assigned value to checks
    to each check division, assigned an unique value with for loop

--- js.js
+++ js.js
@@ -2,7 +2,17 @@ console.log('workin')
 const check = document.getElementsByClassName('check')
 const board = document.getElementById('board')
 
-let count = 0
+let count = 0   // for determinging X or O
+let val = 0
+
+// assigning value to each check divisions
+for(const chk of check) {
+    chk.value = val
+    val++
+}
+
+let player1 = []
+let player2 = []
 
 board.addEventListener('click', event => {
 
@@ -14,16 +24,22 @@ board.addEventListener('click', event => {
 
         if(count%2==0) {
             p.innerText = 'X'
+            player1.push(event.target.value)
             count++
         }else{
             p.innerText = 'O'
+            player2.push(event.target.value)
             count++
         }
         
         event.target.innerHTML = ''
         event.target.appendChild(p)
+        console.log(player1)
+        console.log(player2)
         
 
 })
+
+
 
 
